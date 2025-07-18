8b0c9cd7674705fbdcb0700546cf067703e5e435
rajat <rajatagarwal.109@gmail.com>
2025-07-19 03:38:20 +0530

    test auto deploy on dev

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 require('dotenv').config();
 const app = express();
+const os = require('os');
 
 app.get('/status', (req, res) => {
   res.json({
     status: 'All systems go!',
     env: process.env.APP_ENV || 'development',
     version: process.env.APP_VERSION || '0.0.1',
+    uptime: process.uptime().toFixed(2) + ' seconds',
+    timestamp: new Date().toISOString(),
+    hostname: os.hostname()
   });
 });
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
